a33749483224c6bf1e469d0b57113baeb3b0bb3a
iancu ionut <iiancu85@gmail.com>
2022-02-04 21:08:06 +0200

    Changed cors policy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.options('*', cors())
 
 
-app.use(express.urlencoded({extended: false}));
+// app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 //database
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
